d6e0d8bb2572ed0a043c2f519f9401aec964903b
Shawn Agustin <sragusti@uci.edu>
2021-05-06 17:16:27 -0700

    started front end

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,22 @@ import './App.css';
 const App = () => {
   return(
     <div className="App">
-      Design is my passion.
+      <div className="App-header">
+        <h1> MyMovieList</h1>
+        <div className="Search">
+          <h5>Search for a movie</h5>
+          <input type='text'/>
+        </div>
+        
+      </div>
+      {/* TODO
+      create into different components MODULARIZE*/}
+      <div className="Movies-container">
+        <h2>Plan to watch</h2>
+        <h2>Currently Watching</h2>
+        <h2>Completed</h2>
+      </div>
+      
     </div>
   )
 }
