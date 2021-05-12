cb707fd316a9c507b187e32689723fce15b27845
Shawn Agustin <sragusti@uci.edu>
2021-05-12 13:48:18 -0700

    added display to movies with no poster

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -5,9 +5,18 @@ const Movie = (props) => {
     const IMG_URL = "https://image.tmdb.org/t/p/w1280";
     return(
         <div className="Movie">
-        <img src={IMG_URL + poster_path} alt={title}/>
+        {poster_path ?
+        <img src={IMG_URL + poster_path} alt={title}/> :
+        <div className='No-image'>
+            <p>No image</p>
+        </div>
+        }
+        
         <p>{title}</p>
-        <p>{vote_average}</p>
+        {vote_average === 0 ? <p>No rating</p> :
+            <p>{vote_average}</p>
+        }
+        
         </div>
     )
 }
