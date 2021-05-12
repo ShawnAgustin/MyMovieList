92e25831d62135ea3e7e71bfe14698b3c54f3762
Shawn Agustin <sragusti@uci.edu>
2021-05-12 00:55:40 -0700

    added search functiionality

--- client/src/App.js
+++ client/src/App.js
@@ -11,11 +11,17 @@ import Movie from './components/Movie';
 const App = () => {
 
   const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     axios.get("https://api.themoviedb.org/3/movie/popular?api_key=e9085c32cd25783e01c2ae6ef814c537&language=en-US&page=1")
       .then((res) => setMovies(res.data.results));
   },[])
+
+  const search = () => {
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e9085c32cd25783e01c2ae6ef814c537&language=en-US&query=${query}&page=1&include_adult=false`)
+    .then((res) => setMovies(res.data.results));
+  }
 
   return(
     <div className="App">
@@ -27,12 +33,14 @@ const App = () => {
           <h1>MyMovieList</h1>
           <p>Find movies and keep track and of what you’ve watched and plan to watch.</p>
       </div>
-        <div className="Search">
-          <input type='text'/>
-          <a href="http://google.com"><SearchIcon 
+        <form className="Search">
+          <input type='text' 
+          value={query} 
+          onChange={((e) => setQuery(e.target.value))}/>
+          <SearchIcon 
+          onClick={search}
           style={{position:'relative', right: 35, top: 7.5}}/>
-          </a>
-      </div>
+      </form>
       <div className="Movies-container">
       {movies.map(movie => (
         <Movie {...movie} />
