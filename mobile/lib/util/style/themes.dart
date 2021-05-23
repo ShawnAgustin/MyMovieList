61cfbc8bf46a79f084c771cc5adb0b6cb19baf31
Jam <jjt025@ucsd.edu>
2021-05-23 15:45:18 -0700

    updated theme

--- mobile/lib/util/style/themes.dart
+++ mobile/lib/util/style/themes.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:weeb_dev_my_movie_list/util/helpers/screen_manager.dart';
 
-class Themes{
-
+class Themes {
   static ThemeData _mainTheme = ThemeData(
-    accentColor: Colors.blue,
-    primaryColor: Colors.black,
-    disabledColor: Colors.black45,
-    unselectedWidgetColor: Colors.black45, 
-    textTheme: TextTheme(
-      headline1: TextStyle(
-        color: Colors.black,
-        fontWeight: FontWeight.w700,
-        fontSize: ScreenManager.hp(2)
-      ),
-      subtitle1: TextStyle(
-        color: Colors.black,
-        fontWeight: FontWeight.w400,
-        fontSize: ScreenManager.hp(2)
-      )
-    )
-  );
+      accentColor: Colors.blue,
+      primaryColor: Colors.black,
+      disabledColor: Colors.black45,
+      unselectedWidgetColor: Colors.black45,
+      scaffoldBackgroundColor: Colors.white,
+      backgroundColor: Colors.white,
+      cardColor: Colors.white,
+      canvasColor: Colors.white,
+      textTheme: TextTheme(
+          headline6: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+              fontSize: ScreenManager.hp(4)),
+          headline1: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+              fontSize: ScreenManager.hp(2)),
+          headline2: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+              fontSize: ScreenManager.hp(2.5)),
+          subtitle1: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+              fontSize: ScreenManager.hp(2))));
 
   static ThemeData get mainTheme => _mainTheme;
 }
