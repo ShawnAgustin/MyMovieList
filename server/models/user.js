e253adb9abd6fdc167b83a5cab2f65c76a1cba93
Shawn Agustin <shawnagustin97@gmail.com>
2021-05-28 20:59:00 -0700

    added user model

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username:{
+        type: String,
+        require:true,
+        unique:true,
+        trim: true,
+        minlength:3
+    },
+    planToWatch:{
+        type: Array,
+        default: [],
+    },
+    completed:{
+        type: Array,
+        default: [],
+    }
+    
+});
+
+const User = mongoose.model('User',userSchema);
+
+module.exports= User;
